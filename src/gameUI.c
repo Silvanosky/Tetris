6e7cf3e44c00157cefffff38a63834f22dfd77ed
Sevan Murriguian <murrig_s@epita.fr>
2017-03-22 01:54:39 +0200

    Split fonction

--- src/gameUI.c
+++ src/gameUI.c
@@ -15,6 +15,32 @@ struct sprites* load_sprites()
   return sp;
 }
 
+SDL_Surface* drawScore(SDL_Surface *screen, int score)
+{
+    SDL_Surface *toBlit = NULL;
+    SDL_Rect _score = {20, 20, 0, 0};
+    SDL_Rect _text = {35, 35, 0, 0};
+    toBlit =  SDL_CreateRGBSurface(0, 200, 210, 32, 0, 0, 0, 0);
+    SDL_BlitSurface(toBlit, NULL, screen, &_score);
+
+    TTF_Font *font = NULL;
+    SDL_Color white_color = {255, 255, 255, 0};
+    init_TTF();
+    font = load_font("bin/font/font.ttf", 40);
+    toBlit = TTF_RenderText_Blended(font, "Score : 0", white_color);
+    SDL_BlitSurface(toBlit, NULL, screen, &_text);
+    _text.y += 60;
+    toBlit = TTF_RenderText_Blended(font, "Niveau : 1", white_color);
+    SDL_BlitSurface(toBlit, NULL, screen, &_text);
+    _text.y += 60;
+    toBlit = TTF_RenderText_Blended(font, "Ligne(s) : 0", white_color);
+    SDL_BlitSurface(toBlit, NULL, screen, &_text);
+    SDL_Flip(screen);
+    return screen;
+}
+
+
+
 SDL_Surface* createWindow(SDL_Surface *screen)
 {
   SDL_Surface /**board,*/ *toBlit = NULL;
@@ -26,8 +52,8 @@ SDL_Surface* createWindow(SDL_Surface *screen)
   //board =  SDL_CreateRGBSurface(0, 250, 500, 32, 0, 0, 0, 0);
   //SDL_FillRect(board, NULL, SDL_MapRGB(screen->format, 0, 0, 0));
   //SDL_BlitSurface(board, NULL, screen, &_board);
-  toBlit =  SDL_CreateRGBSurface(0, 200, 210, 32, 0, 0, 0, 0);
-  SDL_BlitSurface(toBlit, NULL, screen, &_score);
+  //toBlit =  SDL_CreateRGBSurface(0, 200, 210, 32, 0, 0, 0, 0);
+  //SDL_BlitSurface(toBlit, NULL, screen, &_score);
   toBlit =  SDL_CreateRGBSurface(0, 100, 100, 32, 0, 0, 0, 0);
   SDL_BlitSurface(toBlit, NULL, screen, &_next);
   //Text
@@ -35,19 +61,20 @@ SDL_Surface* createWindow(SDL_Surface *screen)
   SDL_Color white_color = {255, 255, 255, 0};
   init_TTF();
   font = load_font("bin/font/font.ttf", 40);
-  toBlit = TTF_RenderText_Blended(font, "Score : 0", white_color);
+/*  toBlit = TTF_RenderText_Blended(font, "Score : 0", white_color);
   SDL_BlitSurface(toBlit, NULL, screen, &_text);
   _text.y += 60;
   toBlit = TTF_RenderText_Blended(font, "Niveau : 1", white_color);
   SDL_BlitSurface(toBlit, NULL, screen, &_text);
   _text.y += 60;
   toBlit = TTF_RenderText_Blended(font, "Ligne(s) : 0", white_color);
-  SDL_BlitSurface(toBlit, NULL, screen, &_text);
+  SDL_BlitSurface(toBlit, NULL, screen, &_text);*/
   toBlit = TTF_RenderText_Blended(font, "Next", white_color);
   _next.y += 105;
   _next.x += 17;
   SDL_BlitSurface(toBlit, NULL, screen, &_next);
   SDL_Flip(screen);
+  screen = drawScore(screen, 50);
   return screen;
 }
 
