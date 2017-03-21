b050d381bd77f8fdc0c1dd403f7e4321d8977b4d
Silvanosky <charles.villard@epita.fr>
2017-03-21 22:38:40 +0200

    Start split function

--- src/gameUI.h
+++ src/gameUI.h
@@ -2,9 +2,10 @@
 # define GAMEUI_H_
 
 # include "gui.h"
+# include "game.h"
 
 SDL_Surface* createWindow(SDL_Surface *screen);
 
-SDL_Surface* displayBoard(SDL_Surface *screen, int* board);
+SDL_Surface* displayBoard(SDL_Surface *screen, board* board);
 
 # endif /* GAMEUI_H_ */
