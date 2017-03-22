b78f72ad4fa203091885debbabc5906709b45663
MrColorado <theoissarni@hotmail.fr>
2017-03-22 02:00:29 +0100

    ADD save and load score

--- src/score.c
+++ src/score.c
@@ -1,7 +1,7 @@
 #include "game.h"
 #include "score.h"
 
-void computeScore(int *currentScore, int nbLine, int level) {
+void computeScore (int *currentScore, int nbLine, int level) {
 	switch (nbLine) {
 		case 1: 
 			*currentScore += 40 * (level + 1);
@@ -20,10 +20,78 @@ void computeScore(int *currentScore, int nbLine, int level) {
 	}	
 }
 
-void softDrop(int *currentScore) {
+void softDrop (int *currentScore) {
 	*currentScore += 1;
 }
 
-void hardDrop(int* currentScore, int nbLine) {
+void hardDrop (int *currentScore, int nbLine) {
 	*currentScore += nbLine * 2;
 }
+
+void saveScore (tuple **scoreTab, char *name, int size, int score) {
+	if (score >= scoreTab[9]->score) {
+		tuple *newScore = malloc(sizeof (tuple));
+		newScore->score = score;
+		newScore->name = name;
+		newScore->size = size;
+		scoreTab[9] = newScore;
+
+		int i = 8;
+		while (i > 0 && score >= scoreTab[i]->score) {
+			tuple* save = scoreTab[i];
+			scoreTab[i] = scoreTab[i + 1];
+			scoreTab[i + 1] = save;
+		}
+		
+		FILE *file = fopen("../bin/score.txt", "w");
+		if (file == NULL)
+			errx(1, "error during fopen");
+		
+		for (int i = 0; i < 10; i++) {
+		fwrite(&scoreTab[i]->size, sizeof (int), 1, file);
+		fwrite(scoreTab[i]->name, 1, scoreTab[i]->size, file);
+		fwrite(&scoreTab[i]->score, sizeof (int), 1, file); 
+		}
+		fclose(file);
+	}
+}
+
+tuple** loadScore () {
+	FILE *file = fopen("../bin/score.txt", "r");
+	if (file == NULL)
+		errx(1, "error during fopen");
+	
+	tuple **result = malloc(sizeof (tuple) * 10);
+
+	int cpt = 0;
+	while (cpt < 10) {
+		int *size = malloc(sizeof (int));
+		fread(size, sizeof (int), 1, file);
+		char *name = malloc( sizeof(char) * *size);
+		fread(name, sizeof (char), *size, file);
+		int *score = malloc( sizeof(int));
+		fread(score, sizeof (int), 1, file);
+		
+		tuple *t = malloc(sizeof (tuple));
+		t->size = *size;
+		free(size);
+		t->name = name;
+		t->score = *score;
+		free(score);
+		result[cpt] = t;
+
+		cpt++;
+	}
+	return result;
+}
+
+
+
+
+
+
+
+
+
+
+
--- src/score.h
+++ src/score.h
@@ -2,11 +2,24 @@
 #define FILE_HEADER_SCORE
 
 #include <stdlib.h>
+#include <err.h>
+#include <stdio.h>
 
 void computeScore(int *currentScore, int nbline, int level);
 
 void softDrop(int *currentScore);
 
 void hardDrop(int *currentScore, int nbLine);
+
+struct s_tuple {
+	char *name;
+	int size;
+	int score;
+};
+typedef struct s_tuple tuple; 
+
+void saveScore (tuple **scoreTab, char *name, int size, int score);
+
+tuple** loadScore();
 
 #endif 
